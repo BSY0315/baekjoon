af1bc368c98a80a8570badce6da467e4ac0f29a7
BSY0315 <47170717+BSY0315@users.noreply.github.com>
2021-08-24 21:10:46 +0900

    123

--- __Solving__/1931_meeting_room/1931_meeting_room.cpp
+++ __Solving__/1931_meeting_room/1931_meeting_room.cpp
@@ -2,52 +2,49 @@
 /* 11047 - 회의실 배정 */
 #include <iostream>
 #include <algorithm>
-#include <climits>
+#include <vector>
 
 using namespace std;
 
 typedef unsigned int uint;
 
 int meeting_count;
-pair<uint, uint> meeting_time[100000];
+vector<pair<uint, uint>> meeting_time;
 
 bool compare(const pair<uint, uint> &a, const pair<uint, uint> &b)
 {
-    return a.second < b.second;
+    if (a.second == b.second)
+    {
+        return a.first < b.first;
+    }
+    else
+    {
+        return a.second < b.second;
+    }
 }
 
-bool find_fast_end_meeting(uint &now_time)
+int main(void)
 {
+    cin >> meeting_count;
+    uint start, end;
+    for (int i = 0; i < meeting_count; i++)
+    {
+        cin >> start >> end;
+        meeting_time.push_back(make_pair(start, end));
+    }
+    sort(meeting_time.begin(), meeting_time.end(), compare);
+
+    int max_meeting_count = 0;
+    uint now_time = 0;
     for (int i = 0; i < meeting_count; i++)
     {
         if (meeting_time[i].first >= now_time)
         {
             now_time = meeting_time[i].second;
-            return true;
+            max_meeting_count++;
         }
     }
-    return false;
-}
 
-int main(void)
-{
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-
-    cin >> meeting_count;
-    for (int i = 0; i < meeting_count; i++)
-    {
-        cin >> meeting_time[i].first;
-        cin >> meeting_time[i].second;
-    }
-    sort(meeting_time, meeting_time + meeting_count, compare);
-
-    int max_meeting_count = 0;
-    uint now_time = 0;
-    while (find_fast_end_meeting(now_time))
-    {
-        max_meeting_count++;
-    }
     cout << max_meeting_count;
     return 0;
 }
